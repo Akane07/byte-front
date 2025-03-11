afa39fd347723beedab1da1da6e5e84fe2823bc9
Akane07 <Oreki05@mail.ru>
2025-03-11 17:29:25 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Freelance Byte',
+      htmlAttrs: {
+        lang: 'ru'
+      },
+      link: [{
+        rel: 'icon',
+        href: '/logo.svg',
+        type: 'image/svg+xml'
+      }],
+    }
+  }
+})
