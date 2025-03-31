92247dbb2092a7ab1db0353a8ede296e6cad0602
Akane07 <Oreki05@mail.ru>
2025-03-31 17:43:25 +0300

    button

--- pages/index.vue
+++ pages/index.vue
@@ -60,7 +60,7 @@ onMounted(() => {
             gap: 30px;
             color: #fff;
             text-align: center;
-            margin-top: 100px;
+            margin-top: 250px;
 
             h1 {
                 font-weight: 600;
