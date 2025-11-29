819c059f04f8165cc72cd13d692d2eda91e46af7
Akane07 <Oreki05@mail.ru>
2025-11-29 23:50:36 +0300

    refactor user

--- components/profile/User.vue
+++ components/profile/User.vue
@@ -1,38 +1,40 @@
 <template>
-  <div class="wrapper">
+  <div class="w-full flex justify-center items-center z-100 relative mb-25">
     <DevCard v-if="user">
-      <div class="main_info">
-        <div class="left_part">
-          <div class="avatar">
+      <div class="main_info flex gap-6">
+        <div class="flex flex-col gap-4 min-w-[230px] pb-4.5">
+          <div class="flex flex-col gap-3">
             <UIProfileAvatar :src="makeURL(user?.avatar)"></UIProfileAvatar>
-            <p>{{ user.nickname || "без ника" }}</p>
+            <p>{{ user.nickname || "Ник не задан" }}</p>
           </div>
-          <div class="stats">
-            <div class="stat">
+          <div class="stats flex flex-col gap-2">
+            <div class="flex items-center gap-2">
               <IconsMap></IconsMap>
               <span>{{ user.country || "страна не указана" }}</span>
             </div>
-            <div class="stat">
+            <div class="flex items-center gap-2">
               <IconsClock></IconsClock>
               <span>На сайте с {{ useUserCreated(user.created_at) }}</span>
             </div>
-            <div class="stat">
+            <div class="flex items-center gap-2">
               <IconsSun></IconsSun>
               <span>В сети</span>
             </div>
           </div>
         </div>
         <div class="bordered"></div>
-        <div class="middle_part">
+        <div class="middle_part flex flex-col gap-3 w-full pb-4.5">
           <p class="name">{{ user.name }}</p>
-          <p class="speciality">{{ user.speciality }}</p>
-          <span>{{ user.description || "Нет описания" }}</span>
-          <div class="skills">
+          <p class="font-medium text-[22px]">{{ user.speciality }}</p>
+          <span class="description mt-3">{{
+            user.description || "Нет описания"
+          }}</span>
+          <div class="flex flex-wrap gap-2 pt-3">
             <UIDevChip v-for="skill in user.skills" :text="skill"></UIDevChip>
           </div>
         </div>
         <div class="bordered"></div>
-        <div class="right_part">
+        <div class="w-full flex flex-col gap-8 pb-4.5 pt-3 max-w-[230px]">
           <UIDevButton
             v-if="userStore.user?.id === user.id"
             type="active"
@@ -48,9 +50,12 @@
           >
             Отправить сообщение
           </UIDevButton>
-          <div class="user_stats">
-            <div v-if="userStore.user?.id !== user.id" class="average">
-              <div class="star">
+          <div class="flex flex-col gap-4">
+            <div
+              v-if="userStore.user?.id !== user.id"
+              class="flex flex-col gap-3 text-sm"
+            >
+              <div class="flex items-center gap-2 font-medium font-sm">
                 <img
                   src="../../../assets/images/star.png"
                   alt="star"
@@ -61,21 +66,21 @@
               </div>
               <p>Оценка исполнителя</p>
             </div>
-            <div class="stats">
-              <div class="stat">
-                <span>{{ user.orders_count }}</span>
+            <div class="flex flex-col gap-2">
+              <div class="flex items-center justify-between gap-2">
+                <span class="stat">{{ user.orders_count }}</span>
                 <p>Заказов выполнено</p>
               </div>
-              <div class="stat">
-                <span>70%</span>
+              <div class="flex items-center justify-between gap-2">
+                <span class="stat">70%</span>
                 <p>Успешных заказов</p>
               </div>
-              <div class="stat">
-                <span>{{ user.reviews_count }}</span>
+              <div class="flex items-center justify-between gap-2">
+                <span class="stat">{{ user.reviews_count }}</span>
                 <p>Получено отзывов</p>
               </div>
             </div>
-            <div class="links">
+            <div class="flex flex-col gap-3">
               <div
                 v-if="user.telegram"
                 class="link"
@@ -115,7 +120,7 @@
             Добавить проект в портфолио
           </UIDevButton>
         </div>
-        <div class="portfolio_blocks" v-if="portfolios.length">
+        <div class="flex flex-wrap gap-6" v-if="portfolios.length">
           <ProfilePortfolio
             v-for="portfolio in portfolios"
             :key="portfolio.id"
@@ -178,182 +183,56 @@ onMounted(async () => {
 </script>
 
 <style lang="scss" scoped>
-.wrapper {
-  width: 100%;
+.main_info {
+  border-bottom: 1px solid $border-color;
+
+  .bordered {
+    width: 1px;
+    height: inherit;
+    background: $border-color;
+  }
+
+  .stats, .description {
+    color: $text-secondary;
+    font-size: 14px;
+  }
+
+  .name {
+    font-weight: 600;
+    color: $active;
+    font-size: 24px;
+  }
+
+  .stat {
+    color: $select-enabled;
+    font-weight: 500;
+  }
+
+  .link {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    cursor: pointer;
+    font-weight: 500;
+    font-size: 14px;
+    color: #bebec2;
+  }
+}
+
+.portfolio,
+.feedbacks {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 100;
-  position: relative;
-  margin-bottom: 100px;
+  gap: 40px;
 
-  .main_info {
-    display: flex;
-    gap: 24px;
-    border-bottom: 1px solid $border-color;
-
-    .bordered {
-      width: 1px;
-      height: inherit;
-      background: $border-color;
-    }
-
-    .left_part {
-      display: flex;
-      flex-direction: column;
-      gap: 16px;
-      min-width: 230px;
-      padding-bottom: 18px;
-
-      .avatar {
-        display: flex;
-        flex-direction: column;
-        gap: 12px;
-      }
-
-      .stats {
-        display: flex;
-        flex-direction: column;
-        gap: 8px;
-        color: $text-secondary;
-        font-size: 14px;
-
-        .stat {
-          display: flex;
-          gap: 8px;
-          align-items: center;
-        }
-      }
-    }
-
-    .middle_part {
-      display: flex;
-      flex-direction: column;
-      gap: 12px;
-      padding-bottom: 18px;
-      width: 100%;
-
-      .name {
-        font-weight: 600;
-        color: $active;
-        font-size: 24px;
-      }
-
-      .speciality {
-        font-weight: 500;
-        font-size: 22px;
-      }
-
-      span {
-        margin-top: 12px;
-        font-size: 14px;
-        color: $text-secondary;
-      }
-
-      .skills {
-        padding-top: 12px;
-        margin-top: auto;
-        display: flex;
-        flex-wrap: wrap;
-        gap: 8px;
-      }
-    }
-
-    .right_part {
-      width: 100%;
-      max-width: 230px;
-      display: flex;
-      flex-direction: column;
-      gap: 32px;
-      padding-bottom: 18px;
-      padding-top: 12px;
-
-      .user_stats {
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-
-        .average {
-          display: flex;
-          align-items: center;
-          gap: 12px;
-          font-size: 14px;
-
-          .star {
-            font-weight: 600;
-            display: flex;
-            align-items: center;
-            font-size: 14px;
-            gap: 8px;
-          }
-        }
-
-        .stats {
-          display: flex;
-          flex-direction: column;
-          gap: 8px;
-
-          .stat {
-            display: flex;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            gap: 8px;
-
-            span {
-              color: $select-enabled;
-              font-weight: 500;
-            }
-          }
-        }
-
-        .links {
-          display: flex;
-          flex-direction: column;
-          gap: 12px;
-
-          .link {
-            display: flex;
-            align-items: center;
-            gap: 12px;
-            font-weight: 500;
-            font-size: 14px;
-            color: #bebec2;
-            cursor: pointer;
-
-            P {
-              cursor: pointer;
-            }
-          }
-        }
-      }
-    }
+  p {
+    font-weight: 500;
+    font-size: 22px;
   }
 
-  .portfolio,
-  .feedbacks {
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-
-    p {
-      font-weight: 500;
-      font-size: 22px;
-    }
-
-    .add-button {
-      border-color: $select-enabled;
-      color: $select-enabled;
-    }
-  }
-
-  .portfolio {
-    .portfolio_blocks {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 24px;
-    }
+  .add-button {
+    border-color: $select-enabled;
+    color: $select-enabled;
   }
 }
 </style>
