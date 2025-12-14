a67a56a6a3255734c5f66a9962cecb39f26beda4
Akane07 <Oreki05@mail.ru>
2025-12-14 13:35:31 +0300

    update portfolio

--- pages/profile/portfolio/[id].vue
+++ pages/profile/portfolio/[id].vue
@@ -2,48 +2,53 @@
     <UINavMenu></UINavMenu>
     <UIBackground></UIBackground>
 
-    <div class="wrapper">
-        <div class="portfolio" v-if="portfolio && user && userStore.user">
-            <div class="images">
+    <div class="w-full flex flex-col justify-center items-center z-100 relative mb-25">
+        <div class="portfolio flex flex-col items-center gap-12 w-full max-w-[1440px] mt-8 pt-16 rounded-[20px]"
+            v-if="portfolio && user && userStore.user">
+            <div class="flex flex-col items-center gap-16 px-[42px]">
                 <video v-if="portfolio.video" :src="makeURL(portfolio.video)" controls
-                    style="max-width: 100%; height: auto;"></video>
-                <img class="image" v-for="image in portfolio.images" :key="image" :src="makeURL(image)" alt="">
+                    style="max-width: 100%; height: auto" class="w-full h-auto max-h-[500px] rounded-md"></video>
+                <img class="w-full max-w-[800px] object-cover rounded-md" v-for="image in portfolio.images" :key="image"
+                    :src="makeURL(image)" />
             </div>
-            <div class="info">
+            <div class="flex flex-col items-center gap-8 max-w-[900px] py-[42px]">
                 <div v-if="!portfolio.liked_by.includes(userStore.user.id)" class="like_button" @click="ratePortfolio">
                     <IconsLike></IconsLike>
                 </div>
                 <div v-if="portfolio.liked_by.includes(userStore.user.id)" class="like_button liked"
                     @click="ratePortfolio">
-                    <div class="like_wrapper">
+                    <div class="like_wrapper w-full h-full flex items-center justify-center rounded-[100%]">
                         <IconsLike></IconsLike>
                     </div>
                 </div>
-                <div class="main_info">
-                    <p>{{ portfolio.title }}</p>
-                    <div class="stats">
-                        <div class="stat">
+                <div class="main_info flex flex-col gap-4">
+                    <p class="flex font-semibold text-2xl text-center">
+                        {{ portfolio.title }}
+                    </p>
+                    <div class="flex justify-center gap-4">
+                        <div class="flex items-center gap-2">
                             <IconsEye></IconsEye>
                             <span>{{ portfolio.viewed_by }}</span>
                         </div>
-                        <div class="stat">
+                        <div class="flex items-center gap-2">
                             <IconsSmallLike></IconsSmallLike>
                             <span>{{ portfolio.liked_by.length }}</span>
                         </div>
                     </div>
                     <span>Опубликовано: {{ useUserCreated(portfolio.created_at) }}</span>
                 </div>
-                <div class="description">
+                <div>
                     <p>{{ portfolio.description }}</p>
                 </div>
             </div>
-            <div class="footer">
-                <div class="user">
+            <div class="footer w-full flex flex-col gap-8 mb-25 rounded-md py-10 px-12">
+                <div class="flex items-center gap-4">
                     <UIUserAvatar size="56px" :src="makeURL(user?.avatar)"></UIUserAvatar>
-                    <div class="name">
-                        <p>{{ user?.nickname || 'Без имени' }}</p>
-                        <UIButton v-if="user?.id === userStore.user?.id" active
-                            @click.stop="navigateTo(`/profile/portfolio/edit/${portfolio.id}`)">
+                    <div class="flex flex-col gap-3">
+                        <p class="text-white font-semibold text-[14px]">{{ user?.nickname || "Без имени" }}</p>
+                        <UIButton v-if="user?.id === userStore.user?.id" active @click.stop="
+                            navigateTo(`/profile/portfolio/edit/${portfolio.id}`)
+                            ">
                             <IconsPencil></IconsPencil>
                             Редактировать проект
                         </UIButton>
@@ -53,24 +58,24 @@
                         </UIButton>
                     </div>
                 </div>
-                <div class="portfolio_wrapper" v-if="portfolios">
-                    <div class="other_portfolio">
-                        <ProfilePortfolioBlock v-for="portfolio in portfolios" :portfolio="portfolio" :key="portfolio.id"
-                            :other="true">
+                <div class="relative" v-if="portfolios">
+                    <div class="other_portfolio flex gap-6 overflow-x-auto pb-3">
+                        <ProfilePortfolioBlock v-for="portfolio in portfolios" :portfolio="portfolio"
+                            :key="portfolio.id" other>
                         </ProfilePortfolioBlock>
                     </div>
                 </div>
-                <div class="bottom_info">
-                    <div class="block">
+                <div class="flex justify-between items-start gap-16 pb-10">
+                    <div class="blocks flex flex-col gap-3"> 
                         <p>Роль</p>
                         <div class="value">
                             {{ portfolio.role }}
                         </div>
                     </div>
-                    <div class="block" v-if="portfolio.skills?.length">
+                    <div class="blocks flex flex-col gap-3" v-if="portfolio.skills?.length">
                         <p>Навыки</p>
-                        <div class="value_skill">
-                            <div v-for="skill in portfolio.skills" :key="skill" class="value skill">
+                        <div class="flex flex-wrap gap-3">
+                            <div v-for="skill in portfolio.skills" :key="skill" class="value w-fit!">
                                 {{ skill }}
                             </div>
                         </div>
@@ -82,14 +87,18 @@
 </template>
 
 <script setup lang="ts">
-import { likePortfolio, viewPortfolio, type Portfolio } from "~/shared/api/portfolio-api";
-import type { User } from '~/shared/api/user-api';
-import { makeURL } from '~/shared/utils/helpers';
+import {
+    likePortfolio,
+    viewPortfolio,
+    type Portfolio,
+} from "~/shared/api/portfolio-api";
+import type { User } from "~/shared/api/user-api";
+import { makeURL } from "~/shared/utils/helpers";
 import { usePortfolioStore } from "~/store/portfolioStore";
 import { useUserStore } from "~/store/userStore";
 
 definePageMeta({
-    middleware: ['auth'],
+    middleware: ["auth"],
 });
 
 const route = useRoute();
@@ -97,7 +106,7 @@ const userStore = useUserStore();
 const portfolioStore = usePortfolioStore();
 
 const portfolio = ref<Portfolio>();
-const portfolios = ref<Portfolio[]>();
+const portfolios = ref<Portfolio[]>([]);
 const user = ref<User | null>(null);
 
 async function ratePortfolio() {
@@ -121,218 +130,79 @@ onMounted(async () => {
         user.value = userStore.user;
         await portfolioStore.getMyPortfolio();
         portfolios.value = portfolioStore.portfolio;
-        const index = portfolios.value.findIndex((p) => p.id === id) as number;
-        portfolios.value?.splice(index, 1);
     } else {
         user.value = await userStore.getUserId(portfolio.value.user_id);
-        portfolios.value = await portfolioStore.getPortfoliosById(portfolio.value.user_id);
-        const index = portfolios.value?.findIndex((p) => p.id === id) as number;
-        portfolios.value?.splice(index, 1);
+        portfolios.value = await portfolioStore.getPortfoliosById(
+            portfolio.value.user_id
+        );
     }
+    const index = portfolios.value.findIndex((p) => p.id === id) as number;
+    portfolios.value.splice(index, 1);
 
     await viewPortfolio(id);
 });
 </script>
 
 <style lang="scss" scoped>
+.portfolio {
+    color: $white;
+    background: $bg-brand;
 
-
-.wrapper {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    z-index: 100;
-    position: relative;
-    margin-bottom: 100px;
-
-    .portfolio {
-        color: $white;
+    .like_button {
+        width: 64px;
+        height: 64px;
         display: flex;
-        flex-direction: column;
         align-items: center;
-        gap: 48px;
-        width: 100%;
-        max-width: 1440px;
-        margin-top: 32px;
-        background: $bg-brand;
-        border-radius: 20px;
-        padding: 64px 0 0 0;
+        justify-content: center;
+        background: $primary;
+        cursor: pointer;
+        padding: 2px;
+        border-radius: 100%;
 
-        .images {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 64px;
-            padding: 0 42px;
-
-            .image {
-                width: 100%;
-                max-width: 800px;
-                object-fit: cover;
-                border-radius: 6px;
-                // max-height: 600px;
-            }
-
-            video {
-                width: 100%;
-                border-radius: 6px;
-                max-height: 500px;
-            }
+        svg {
+            cursor: pointer;
         }
 
-        .info {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 32px;
-            max-width: 900px;
-            padding: 0 42px;
+        &.liked {
+            background: $bg-button-gradient;
 
-            .like_button {
-                border-radius: 100%;
-                width: 64px;
-                height: 64px;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                background: $primary;
-                cursor: pointer;
-                padding: 2px;
-
-                svg {
-                    cursor: pointer;
-                }
-
-                &.liked {
-                    background: $bg-button-gradient;
-
-                    .like_wrapper {
-                        width: 100%;
-                        height: 100%;
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                        background: $input-auth;
-                        border-radius: 100%;
-                    }
-                }
+            .like_wrapper {
+                background: $input-auth;
             }
+        }
+    }
 
-            .main_info {
-                display: flex;
-                flex-direction: column;
-                gap: 16px;
+    span {
+        color: $text-secondary;
+        text-align: center;
+        font-size: 14px;
+    }
 
-                &>p {
-                    font-weight: 600;
-                    font-size: 24px;
-                    text-align: center;
-                }
+    .footer {
+        background: $tag-color;
 
-                span {
-                    font-size: 14px;
-                    color: $text-secondary;
-                    text-align: center;
-                }
+        .other_portfolio {
+            scroll-snap-type: x mandatory;
+            scroll-behavior: smooth;
 
-                .stats {
-                    display: flex;
-                    gap: 16px;
-                    justify-content: center;
-
-                    .stat {
-                        display: flex;
-                        gap: 8px;
-                        align-items: center;
-                    }
-                }
-            }
-
+            @include custom-scrollbar;
         }
 
-        .footer {
-            width: 100%;
-            background: $tag-color;
-            border-radius: 6px;
-            margin-bottom: 100px;
-            display: flex;
-            flex-direction: column;
-            gap: 32px;
+        .blocks {
+            width: calc(50% - 32px);
 
-            .user {
-                display: flex;
-                align-items: center;
-                gap: 16px;
-                padding: 40px 48px 0 48px;
-
-                .name {
-                    display: flex;
-                    flex-direction: column;
-                    gap: 12px;
-
-                    p {
-                        color: $white;
-                        font-weight: 600;
-                        font-size: 14px;
-                    }
-                }
+            p {
+                font-weight: 500;
+                font-size: 14px;
             }
 
-            .portfolio_wrapper {
-                position: relative;
-                margin: 0 40px 0 48px;
-
-                .other_portfolio {
-                    display: flex;
-                    gap: 24px;
-                    overflow-x: auto;
-                    scroll-snap-type: x mandatory;
-                    scroll-behavior: smooth;
-                    padding-bottom: 12px;
-
-                    @include custom-scrollbar; 
-                }
-            }
-
-            .bottom_info {
-                display: flex;
-                align-items: start;
-                justify-content: space-between;
-                gap: 64px;
-                padding: 0 48px 40px 48px;
-
-                .block {
-                    display: flex;
-                    flex-direction: column;
-                    gap: 12px;
-                    width: calc(50% - 32px);
-
-                    p {
-                        font-weight: 500;
-                        font-size: 14px;
-                    }
-
-                    .value_skill {
-                        display: flex;
-                        flex-wrap: wrap;
-                        gap: 12px;
-                    }
-
-                    .value {
-                        width: 100%;
-                        color: $text-secondary;
-                        font-size: 12px;
-                        padding: 12px 28px;
-                        border-radius: 6px;
-                        background: $input-auth;
-
-                        &.skill {
-                            width: fit-content;
-                        }
-                    }
-                }
+            .value {
+                width: 100%;
+                color: $text-secondary;
+                font-size: 12px;
+                padding: 12px 28px;
+                border-radius: 6px;
+                background: $input-auth;
             }
         }
     }
